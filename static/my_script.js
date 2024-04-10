0ff78686d3464a6f2a711db7b10ef44a88b1d7ec
yasheks <yakovzeld@gmail.com>
2024-04-10 21:10:55 +0300

    Add files via upload
    Signed-off-by: yasheks <yakovzeld@gmail.com>

---
+++ static/my_script.js
@@ -0,0 +1,15 @@
+var arrLang ={
+    ru:{
+        'FILTERS': 'ФИЛЬТРЫ',
+        'gender': 'пол участников'
+        'number': 'кол-во участников'
+        'search_bar': 'результаты поиска'
+        'zero': 'Нет доступных видео'
+    },
+    en:{
+        'FILTERS': 'FILTERS',
+        'gender': 'gender'
+        'number': 'number'
+        "search_bar": 'Search for the result'
+        'zero': 'empty'
+    },
