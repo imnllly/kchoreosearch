e3a227e3a78278bda17a3011dd8b2eeda892a8ff
yasheks <143996832+yasheks@users.noreply.github.com>
2024-04-13 02:22:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,21 @@ app = Flask(__name__)
 filter_groups = [
     {
         "title": "gender",
+        "translate": "пол комманды",
         "filters": [
             {
                 "name": "BOY GROUP",
+                "translate": "МУЖСКАЯ ГРУППА",
                 "code": 'male'
             },
             {
                 "name": "GIRL GROUP",
+                "translate": "ЖЕНСКАЯ ГРУППА",
                 "code": 'female'
             },
             {
                 "name": "SWITCH GROUP",
+                "translate": "СМЕШАНАЯ ГРУППА",
                 "code": 'switch'
             }
         ]
@@ -33,26 +37,32 @@ filter_groups = [
             },
             {
                 "name": "2 MEMBERS",
+                "translate": "2 УЧАСТНИКА",
                 "code": 'members_2'
             },
             {
                 "name": "3 MEMBERS",
+                "translate": "3 УЧАСТНИКА",
                 "code": 'members_3'
             },
             {
                 "name": "4 MEMBERS",
+                "translate": "4 УЧАСТНИКА",
                 "code": 'members_4'
             },
             {
                 "name": "5 MEMBERS",
+                "translate": "5 УЧАСТНИКОВ",
                 "code": 'members_5'
             },
             {
                 "name": "6 MEMBERS",
+                "translate": "6 УЧАСТНИКОВ",
                 "code": 'members_6'
             },
             {
                 "name": "7 MEMBERS",
+                "translate": "7 УЧАСТНИКОВ",
                 "code": 'members_7'
             },
         ]
