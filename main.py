446b7f0bdcc226d2cfa393a581ea80a4dd8f9a36
yasheks <yakovzeld@gmail.com>
2024-05-08 21:20:27 +0300

    BETA

--- main.py
+++ main.py
@@ -99,19 +99,29 @@ def index():
 
 @app.route('/translate')
 def translate():
-    search_query = request.args.get('q') or ""
+    search_query = request.args.get('q', "")
     with sqlite3.connect("horeo.db") as conn:
         cur = conn.cursor()
+        print(search_query)
         cur.execute("SELECT * FROM groups WHERE `group` like ?", ('%' + search_query + '%',))
         videos = cur.fetchall()
 
         filtered_videos = []
+        page = int(request.args.get('page', 0))
+
+        filter_values = dict(request.args)
+        if 'page' in filter_values:
+            del filter_values['page']
+
 
         for video in videos:
-            if len(request.args) <= 1 or set(video[6].split(',')).intersection(set(request.args)):
+            if len(filter_values) <= 1 or set(video[6].split(',')).intersection(set(filter_values)):
                 filtered_videos.append(video[4])
 
-        return render_template('translate.html', videos=filtered_videos, filter_groups=filter_groups)
+        pages = math.ceil(len(filtered_videos) / 12)
+
+        filtered_videos = filtered_videos[12 * page: 12 * (page + 1)]
+        return render_template('translate.html', videos=filtered_videos, filter_groups=filter_groups, pages=pages, page=page)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
