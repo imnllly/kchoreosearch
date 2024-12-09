a02065e2159ec42ee05f3f58896c341cd942bba6
CectAss <npnbem.fm@gmail.com>
2024-12-09 21:31:29 +0300

    anti normis system

---
+++ config.py
@@ -0,0 +1,64 @@
+filter_groups = [
+    {
+        "title": "gender",
+        "translate_title": "группы",
+        "filters": [
+            {
+                "name": "BOY GROUP",
+                "translate": "МУЖСКАЯ ГРУППА",
+                "code": 'male'
+            },
+            {
+                "name": "GIRL GROUP",
+                "translate": "ЖЕНСКАЯ ГРУППА",
+                "code": 'female'
+            },
+            { 
+                "name": "SWITCH GROUP",
+                "translate": "СОВМЕСТНАЯ ГРУППА",
+                "code": 'switch'
+            }
+        ]
+    },
+    {
+        "title": "number",
+        "translate_title": "кол-во участников",
+        "filters": [
+            {
+                "name": "1 MEMBER",
+                "translate": "1 УЧАСТНИК",
+                "code": 'members_1'
+            },
+            {
+                "name": "2 MEMBERS",
+                "translate": "2 УЧАСТНИКа",
+                "code": 'members_2'
+            },
+            {
+                "name": "3 MEMBERS",
+                "translate": "3 УЧАСТНИКА",
+                "code": 'members_3'
+            },
+            {
+                "name": "4 MEMBERS",
+                "translate": "4 УЧАСТНИКА",
+                "code": 'members_4'
+            },
+            {
+                "name": "5 MEMBERS",
+                "translate": "5 УЧАСТНИКОВ",
+                "code": 'members_5'
+            },
+            {
+                "name": "6 MEMBERS",
+                "translate": "6 УЧАСТНИКОВ",
+                "code": 'members_6'
+            },
+            {
+                "name": "7 MEMBERS",
+                "translate": "7 УЧАСТНИКОВ",
+                "code": 'members_7'
+            },
+        ]
+    }
+]
