9ebefca1050caec927676800d289212e2faae870
yakov <yakovzeld@gmail.com>
2024-12-11 18:31:17 +0300

    not_working(

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import math
-from flask import render_template, request, Blueprint
+from flask import render_template, request, Blueprint, session, redirect
 from config import *
 from db import db
 
@@ -8,7 +8,11 @@ connect = db("postgres")
 
 @main.route('/')
 def index():
-
+    if 'language' in session:
+        language = session['language']
+    else:
+        language = "ru"
+    
     search_query = request.args.get('q', "")
         
     videos = connect.select("SELECT * FROM groups WHERE group_name LIKE '%"+search_query+"%';")
@@ -34,37 +38,22 @@ def index():
 
     filtered_videos = filtered_videos[12 * page: 12 * (page + 1)]
 
-    return render_template('tasks.html', videos=filtered_videos, filter_groups=filter_groups, pages=pages, page=page)
+    if language == "ru":
+
+        return render_template('index.html', videos=filtered_videos, filter_groups=filter_groups, pages=pages, page=page)
+    else:
+        return render_template('translate.html', videos=filtered_videos, filter_groups=filter_groups, pages=pages, page=page)
 
 
 
 @main.route('/translate')
 def translate():
 
-    search_query = request.args.get('q', "")
-        
-    connect = db("postgres")
-    videos = connect.select("SELECT * FROM groups WHERE group_name LIKE '%"+search_query+"%';")
+    session["language"] = "en"
+    return redirect("/")
 
-    filtered_videos = []
-    page = int(request.args.get('page', 0))
-    filter_values = dict(request.args)
+@main.route('/rus')
+def translate():
 
-
-    if 'page' in filter_values:
-
-        del filter_values['page']
-
-
-    for video in videos:
-
-        if len(filter_values) <= 1 or set(video[6].split(',')).intersection(set(filter_values)):
-
-            filtered_videos.append(video[4])
-
-
-    pages = math.ceil(len(filtered_videos) / 12)
-
-    filtered_videos = filtered_videos[12 * page: 12 * (page + 1)]
-
-    return render_template('translate.html', videos=filtered_videos, filter_groups=filter_groups, pages=pages, page=page)
+    session["language"] = "ru"
+    return redirect("/")
