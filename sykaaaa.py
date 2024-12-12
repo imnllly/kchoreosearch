4f896111ecdb804fb3bd6e0ff79ca06889a67b59
CectAss <npnbem.fm@gmail.com>
2024-12-12 23:38:23 +0300

    misc

--- sykaaaa.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://www.youtube.com/watch?v=8j29aAbtYWo"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-title = soup.find('span', attrs={'class': 'watch-title'})
-print(title.text)
-
